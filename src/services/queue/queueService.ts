bcfdb0ef1249fecece9cf57536b5cef8cc144b5f
Jullie8 <julissale8@gmail.com>
2026-02-25 21:03:24 -0500

    scaffold queueService with initial method stubs

---
+++ src/services/queue/queueService.ts
@@ -0,0 +1,16 @@
+// TODO: scaffold queueService module
+
+// addPerson
+const addPerson = () => {};
+
+// removePerson
+const removePerson = () => {}
+
+// assignComputer
+const assignComputer = () => {}
+
+export const queueService = {
+  addPerson,
+  removePerson,
+  assignComputer
+}
