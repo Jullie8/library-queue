301a647989caac6c636146868acf2749efb82f8f
Jullie8 <julissale8@gmail.com>
2026-04-06 21:05:00 -0400

    implement load and remove methods in localStorageAdapter

--- src/services/storage/localStorageAdapter.ts
+++ src/services/storage/localStorageAdapter.ts
@@ -1,5 +1,5 @@
 
-// save
+// save => write data
 const save = <T> (key: string, data:T): void  => {
   try {
     if (data === undefined) return;
@@ -10,10 +10,39 @@ const save = <T> (key: string, data:T): void  => {
   }
 }
 
-// load
+// load => retrieves stored data reads data
+const load = <T>(key: string, data: T): T => {
+  try {
+    const FULL_KEY = `libraryQueue:${key}`;
+    const raw = localStorage.getItem(FULL_KEY); //either string or null
+    
+    if (!raw) return data; // key missing
 
-// remove
+    const parsed = JSON.parse(raw);
+
+    if (parsed === null) return data; // key exists but value is null
+    
+
+    return parsed;
+  } catch(error) {
+    console.error("Storage load error", key, error);
+    return data;
+  }
+}
+
+// remove => Delete one stored item using its key.
+const remove = (key: string): void =>  {
+  try {
+    const FULL_KEY = `libraryQueue:${key}`;
+
+    localStorage.removeItem(FULL_KEY);
+  } catch (error) {
+      console.error("Storage remove error", key, error);
+  }
+}
 
 export const localStorageAdapter = {
-  save
+  save, 
+  load,
+  remove
 };
