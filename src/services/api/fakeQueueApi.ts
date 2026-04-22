c128d84304ae073e17b41387858574f6f1a5e4b7
Jullie8 <julissale8@gmail.com>
2026-04-22 15:19:38 -0400

    cleanup

--- src/services/api/fakeQueueApi.ts
+++ src/services/api/fakeQueueApi.ts
@@ -1,7 +1,7 @@
 import type { QueuePerson } from "../../features/queue/types";
 import { queueService } from "../queue/queueService"
 
- const getQueue = () : Promise<QueuePerson[]> => {
+const getQueue = () : Promise<QueuePerson[]> => {
   // return a promise
   return new Promise((resolve) => {
     // wait 300ms
@@ -10,9 +10,9 @@ import { queueService } from "../queue/queueService"
       resolve(queue);
     }, 300);
   });
- }
+}
 
- const addPerson = (libraryCardLastFour: string, name: string) : Promise<QueuePerson[]> => {
+const addPerson = (libraryCardLastFour: string, name: string) : Promise<QueuePerson[]> => {
   return new Promise((resolve) => {
     setTimeout(() => {
       const queue = queueService.addPerson(
@@ -29,18 +29,15 @@ const removePerson = (id: string) : Promise<QueuePerson[]> => {
     // wait 300ms THEN: 
     setTimeout(() => {
       const queue = queueService.removePerson(id);
-
       resolve(queue);
     }, 300);
   });
-
 }
 
 const assignComputer = (id: string, computerNumber: number) : Promise<QueuePerson[]> => {
   return new Promise((resolve) => {
     setTimeout(() => {
       const queue = queueService.assignComputer(id, computerNumber);
-
       resolve(queue);
     }, 300);
   });
