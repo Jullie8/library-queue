bb2d91209bd466c1231123dfed1b5e2e7efbd533
Jullie8 <julissale8@gmail.com>
2026-04-22 15:15:56 -0400

    adds fakeQueueApi removePerson & assign Computer with delay

--- src/services/api/fakeQueueApi.ts
+++ src/services/api/fakeQueueApi.ts
@@ -1,13 +1,11 @@
 import type { QueuePerson } from "../../features/queue/types";
 import { queueService } from "../queue/queueService"
 
-
  const getQueue = () : Promise<QueuePerson[]> => {
   // return a promise
   return new Promise((resolve) => {
     // wait 300ms
     setTimeout(() => {
-      // load queue Service
       const queue = queueService.getQueue();
       resolve(queue);
     }, 300);
@@ -24,9 +22,33 @@ import { queueService } from "../queue/queueService"
       resolve(queue);
     }, 300);
   });
- }
+}
+
+const removePerson = (id: string) : Promise<QueuePerson[]> => {
+  return new Promise((resolve) => {
+    // wait 300ms THEN: 
+    setTimeout(() => {
+      const queue = queueService.removePerson(id);
+
+      resolve(queue);
+    }, 300);
+  });
+
+}
+
+const assignComputer = (id: string, computerNumber: number) : Promise<QueuePerson[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const queue = queueService.assignComputer(id, computerNumber);
+
+      resolve(queue);
+    }, 300);
+  });
+}
 
 export const fakeQueueApi = {
   getQueue,
   addPerson,
-}
+  removePerson,
+  assignComputer,
+};
