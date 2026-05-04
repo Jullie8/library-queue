9974eddf9e7a70c368de139ab2ee23d819f0d4d5
Jullie8 <julissale8@gmail.com>
2026-05-04 16:32:50 -0400

    implement store and fetch logic

--- src/features/queue/queueStore.ts
+++ src/features/queue/queueStore.ts
@@ -1,20 +1,45 @@
 
 import { create } from "zustand";
 import type { QueuePerson } from "./types";
+import { fakeQueueApi } from "../../services/api/fakeQueueApi";
 
 type State = {
- queue: QueuePerson[];
- isLoading : boolean;
- error: string | null;
+  queue: QueuePerson[];
+  isLoading : boolean;
+  error: string | null;
+  actions: QueueActions; 
 };
 
-export const useQueueStore = create<State>((set) => {
-  // return object all the diff values you want in the store
+type QueueActions = {
+    fetchQueue: () => Promise<void>;
+};
+
+const useQueueStore = create<State>((set) => {
   return { 
     queue: [],
     isLoading : false,
     error: null,
+    actions: {
+      fetchQueue: async () => {
+        set({ isLoading: true, error: null });
+        try {
+          const data  = await fakeQueueApi.getQueue();
+          set({ queue: data });
+        } catch {
+          set({ error: "Failed to fetch" })
+        } finally {
+            set({ isLoading: false });
+        }
+      }
+    }
   }
 });
 
+// Selectors for data
+export const useQueue = () => useQueueStore((state) => state.queue);
+export const useQueueLoading = () => useQueueStore((state) => state.isLoading);
+
+
+// one selector for all our actions
+export const useQueueActions = () => useQueueStore((state) => state.actions);
 
